857cc0989ca7fb86036726339000d67edb7909a2
Ravi Kumar <ravi181997@gmail.com>
2025-12-16 19:57:19 +0530

    Initial Commit

---
+++ PSKiller/PSKillerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSKillerApp.swift
+//  PSKiller
+//
+//  Created by Ravi Kumar on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PSKillerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
